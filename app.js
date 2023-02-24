2bf38a04278324dde3e979508d7e7b98d38682b5
max5961 <mmorrissey5961@gmail.com>
2023-02-24 00:25:48 -0500

    Add logic operator buttons to remove eval() function.

--- app.js
+++ app.js
@@ -1,72 +1,112 @@
-let oneOperand = '';
-let twoOperand = '';
-let threeOperand = '';
+let valueOne = '';
+let valueTwo = '';
+let valueThree = null;
+console.log(valueOne != valueThree);
+operatorOrder = [];
 
 
-
-inputNumbers = true;
-content = document.getElementById('console');
+let inputValueOne = true;
+let inputNumbers = true;
+let content = document.getElementById('console');
+let outputValue = document.getElementById('outputValue');
 
 //numbers and parentheses
-one = document.getElementById('one');
-two = document.getElementById('two');
-three = document.getElementById('three');
-four = document.getElementById('four');
-five = document.getElementById('five');
-six = document.getElementById('six');
-seven = document.getElementById('seven');
-eight = document.getElementById('eight');
-nine = document.getElementById('nine');
-zero = document.getElementById('zero');
-leftP = document.getElementById('leftP');
-rightP = document.getElementById('rightP');
-decimalPoint = document.getElementById('decimalPoint');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
+const leftP = document.getElementById('leftP');
+const rightP = document.getElementById('rightP');
+const decimalPoint = document.getElementById('decimalPoint');
 
 //operators
-divide = document.getElementById('divide');
-multiply = document.getElementById('multiply');
-subtract = document.getElementById('subtract');
-add = document.getElementById('add');
-modulo = document.getElementById('modulo');
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const subtract = document.getElementById('subtract');
+const add = document.getElementById('add');
+const modulo = document.getElementById('modulo');
 
-//equals
-equals = document.getElementById('equals');
-equals.addEventListener('click', () => {
-    content.textContent = eval(content.textContent);
-    inputNumbers = false;
-
-})
-
-clear = document.getElementById('clear');
+//equals and clear
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear');
 
 numberButtons = [one, two, three, four, five, six, seven, eight, nine, zero, rightP, leftP, decimalPoint];
 numberButtons.forEach(number => {
     console.log(number);
     number.addEventListener('click', () => {
-        if(content.textContent.length < 15 && inputNumbers){
+        if(content.textContent.length < 25 && inputNumbers && (valueOne != valueThree)){
             content.textContent += number.textContent;
+            valueOne += number.textContent;
+            console.log(`from # click, inputValueOne should be true: ${inputValueOne}, valueOne: ${valueOne}`);
+            console.log(`from # click, inputValueOne should be true: ${inputValueOne}, valueTwo: ${valueTwo}`);
+        } else if (content.textContent.length < 25 && inputNumbers){
+            content.textContent += number.textContent;
+            valueTwo += number.textContent
+            console.log(`from # click, inputValueOne should be false: ${inputValueOne}, valueOne: ${valueOne}`)
+            console.log(`from # click, inputValueOne should be false: ${inputValueOne}, valueTwo: ${valueTwo}`)
         }
-    })
-})
 
-operators = [add, divide, multiply, subtract, modulo]
+        //outputBox.textContent = operate(valueOne, value2, operator)
+        })
+    })
+
+operators = [add, divide, multiply, subtract]
 
 operators.forEach(operator => {
     operator.addEventListener('click', () => {
-        if(content.textContent != ''){
+        if((content.textContent != '') && (content.textContent.slice(-2,-1) != '+') &&
+        (content.textContent.slice(-2,-1) != '-') &&
+        (content.textContent.slice(-2,-1) != '*') &&
+        (content.textContent.slice(-2,-1) != '/')){
             content.textContent += ` ${operator.textContent} `;
-        inputNumbers = true;
+            inputNumbers = true;
+            inputValueOne = !inputValueOne;
+            operatorOrder.push(operator.textContent)
+            valueThree = operate(valueOne, valueTwo, valueThree, operatorOrder);
+            outputValue.textContent = valueThree;
+            valueOne = valueThree;
+            valueTwo = '';
+            
+            console.log(`from OPERATOR click, inputValueOne should be false: ${inputValueOne}, valueOne: ${valueOne}`);
+            console.log(`from OPERATOR click, inputValueOne should be false: ${inputValueOne}, valueTwo: ${valueTwo}`);
         }
     })
 })
 
+function operate(value1, value2, value3, operatorOrder){
+    if(operatorOrder.length == 1){
+        operator = operatorOrder[0]
+        if(operator == '+'){
+            value3 = Number(value1) + Number(value2);
+        } else if(operator == '-'){
+            value3 = Number(value1) - Number(value2);
+        } else if(operator == '*'){
+            value3 = Number(value1) * Number(value2);
+        } else if(operator == '/'){
+            value3 = Number(value1) / Number(value2);
+        }
+    } else if(operatorOrder.length > 1){
+        console.log(operatorOrder[operatorOrder.length - 2]);
 
-function clearScreen(){
-    content.textContent = '';
-    inputNumbers = true;
+        if(operatorOrder[operatorOrder.length - 2] == '+'){
+            value3 = Number(value1) + Number(value2);
+        } else if(operatorOrder[operatorOrder.length - 2] == '-'){
+            value3 = Number(value1) - Number(value2);
+        } else if(operatorOrder[operatorOrder.length - 2] == '*'){
+            value3 = Number(value1) * Number(value2);
+        } else if(operatorOrder[operatorOrder.length - 2] == '/'){
+            value3 = Number(value1) / Number(value2);
+        }
+    }
+
+    return value3;
 }
-
-clear.addEventListener('click', clearScreen);
 
 
 
